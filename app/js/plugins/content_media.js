daacbff7a5c6ef5077d1b5075be7949fe7bc6b58
Paulo Almeida <paulo.cc03@gmail.com>
2013-11-19 18:19:35 +0700

    Galleria implemented. first scratch

--- app/js/plugins/content_media.js
+++ app/js/plugins/content_media.js
@@ -6,101 +6,13 @@
 	app.controller("ContentMediaCtrl", ['$scope', '$location', 'steam',
 	function($scope, loc, steam) {
 
-		// I flag the visibility of the big box.
-		$scope.isBoxVisible = true;
+		$scope.medias = listOfContent;
 
-		$scope.categories = listOfCategories;
-
-		// ---
-		// PUBLIC METHODS.
-		// ---
-		$scope.activateSection = function(id){
-			//First we must obtain the list from steam
-			//TODO Steam call to obtain list of categories
-			$scope.activeTitle = listOfCategories[id].title;
-			console.log('ACTIVE TITLE: ',$scope.activeTitle);
-			$scope.activeId =listOfCategories[id].id;
-			$scope.activeDescription =listOfCategories[id].description;
-			$scope.media = buildPhotoSet(listOfCategories[id].id);
-		}
-		
-
-		// ---
-		// PRIVATE METHODS.
-		// ---
-		function buildPhotoSet(id) {
-			var media = [];
-			
-			//TODO Steam call to obtain list of media for 1 category
-			/*
-			for (var i = 0; i < listOfContent.length; i++) {
-				if (i % 3 == 0){
-					photos.push([]);
-				}
-				photos[photos.length - 1].push({
-						id : listOfContent[i].id,
-						title : listOfContent[i].title,
-						src : listOfContent[i].src,
-						type : listOfContent[i].type,
-						description : listOfContent[i].description
-					});
-			}*/
-
-			$.each(listOfContent, function(key, value){
-				media.push({
-						id : value.id,
-						title : value.title,
-						src : value.src,
-						type : value.type,
-						description : value.description
-					});
-			});
-
-			return (media);
-		}
-
-		//need to think in where I'm going to catch this id
-		return $scope.activateSection(0);
+		Galleria.loadTheme('lib/galleria/themes/classic/galleria.classic.min.js');
+		Galleria.run('[ng-controller="ContentMediaCtrl"] .galleria');
 
 	}]);
 
-	var listOfCategories = [
-		{
-		'title':'Logos',
-		'id':'0',
-		'description' : 'All the logos in the world.'
-		},
-		{
-		'title':'Seed Round',
-		'id':'1',
-		'description' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, '
-		},
-		{
-		'title':'Investors',
-		'id':'2',
-		'description' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, '
-		},
-		{
-		'title':'Plans',
-		'id':'3',
-		'description' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, '
-		},
-		{
-		'title':'Budget',
-		'id':'4',
-		'description' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, '
-		},
-		{
-		'title':'Offices',
-		'id':'5',
-		'description' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, '
-		},
-		{
-		'title':'Hiring',
-		'id':'6',
-		'description' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, '
-		},
-	];
 	var listOfContent = [
 		{
 		'id' : '0',
@@ -125,15 +37,15 @@
 		'type' : 'img',
 		'title' : 'IBM',
 		'description' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, '
-		},
-		{
-		'id' : '4',
-		'rootId' : '1',
-		'src' : 'http://clips.vorwaerts-gmbh.de/VfE_html5.mp4',
-		'type' : 'video',
-		'title' : 'How to...',
-		'description' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, '
 		}
+		// ,{
+		// 'id' : '4',
+		// 'rootId' : '1',
+		// 'src' : 'http://clips.vorwaerts-gmbh.de/VfE_html5.mp4',
+		// 'type' : 'video',
+		// 'title' : 'How to...',
+		// 'description' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, '
+		// }
 	];
 
 }).call(this);
