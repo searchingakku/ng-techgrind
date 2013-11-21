1d583c182163455e19daad8a562290090135b51d
efraimip <efraim@ekita.co>
2013-11-22 02:33:37 +0700

    fullpage styling

--- app/js/services/content_article_fullpage.js
+++ app/js/services/content_article_fullpage.js
@@ -1,12 +1,21 @@
 var myAppModule = angular.module('ContentFullPageModal', ['ui.bootstrap','TechGrindApp.controllers']);
 myAppModule.service('ContentFullPageModalService', ['$dialog', 'steam','$rootScope',
 function($dialog, steam, $rootScope) {
-
+	var contentdata = {
+		author:'Efraim Pettersson',
+		source:'http://efraimip.blogspot.com',
+		date:'12-03-2013',
+		time:'18:06',
+		twitterid:'@EfraimIP',
+		websiteurl:'www.grinder.com',
+		title:'Test Title....',
+		content:'we rowaej roiwaej roiwaej roijaweo rawo hriuaw ehriuawh eriu haweiurhawilu rhw e'
+	};
 	this.open = function(id) {
 
-		dialogOptions = {
+		var dialogOptions = {
 			title : 'my test title....'
-		}
+		};
 
 		var dialogDefaults = {
 			backdrop : true,
@@ -14,19 +23,15 @@ function($dialog, steam, $rootScope) {
 			backdropClick : true,
 			dialogFade : true,
 			templateUrl : 'partials/services/content_article_fullpage.html',
-			dialogClass : 'modal FullPage'
+			dialogClass : 'modal'
 		};
-		
 		dialogDefaults.controller = function($scope, dialog) {
 			$scope.dialogOptions = dialogOptions;
 			$scope.dialogOptions.close = function(result) {
 				dialog.close(result);
 			};
 		}
-
 		var d = $dialog.dialog(dialogDefaults);
 		d.open();
-
 	};
-
-}]); 
+}]);
