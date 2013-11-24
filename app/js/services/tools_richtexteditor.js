de509e1b712d719fa71b5f863c4c489a6b74809d
Paulo Almeida <paulo.cc03@gmail.com>
2013-11-24 20:17:09 +0700

    RichEditor fixed, still missing the editing part

--- app/js/services/tools_richtexteditor.js
+++ app/js/services/tools_richtexteditor.js
@@ -1,157 +1,149 @@
+(function() {
+	var app = angular.module('ToolsRichEditor', [ 'ui.bootstrap' ]);
+	app.service('ToolsRichEditorService',
+		['$modal', 'steam', '$rootScope',
+		 function($modal, steam, $rootScope) {
 
-var appModule = angular.module('ToolsRichEditor', []);
+			var self = this;
+			self.isOpen = false;
+			self.isLogged = false;
+			self.user = steam.loginp();
+			self.loginp = steam.loginp;
 
-appModule.service('ToolsRichEditorService', ['$modal', 'steam', '$rootScope',
-	function($modal, steam, $rootScope) {
+//			self.dialogCategories = [ {
+//				name : 'news'
+//			}, {
+//				name : 'guides'
+//			}, {
+//				name : 'tutorials'
+//			} ];
+			self.dialogCategories = ['news','guides','tutorials'];
+			var user = null;
 
-		var self = this;
-		var elementID = 'inputContentRichText';
+			this.open = function() {
 
-		self.isOpen = false;
-
-		var dialogDefaults = {
-			backdrop : false,
-			keyboard : true,
-			backdropClick : true,
-			dialogFade : true,
-			templateUrl : 'partials/services/tools_richeditor.html',
-			dialogClass : 'modal myWindow'
-		};
-
-		var dialogOptions = {
-			closeButtonText : 'Cancel',
-			actionButtonText : 'Publish',
-			saveButtonText : 'Save',
-			loginButtonText : 'Login',
-			isLoged : false,
-			title : '',
-			category : '',
-			labels : '',
-			fullText : ''
-		};
-
-		var dialogCategories = [
-			'news',
-			'guides',
-			'tutorials'
-		];
-
-		var user = null;
-
-		this.open = function(customDialogDefaults, customDialogOptions) {
-			if (!self.isOpen) {
 				self.isOpen = true;
-				if (!customDialogDefaults) {
-					customDialogDefaults = {};
-				}
+				// lets check if the user is logged
+				self.user = steam.loginp();
+				self.loginp = steam.loginp;
+
+				var modalInstance = $modal.open({
+					backdrop : false,
+					keyboard : true,
+					backdropClick : true,
+					dialogFade : true,
+					templateUrl : 'partials/services/tools_richeditor.html',
+					controller : ModalInstanceCtrl,
+					windowClass : 'modal myWindow',
+					resolve : {
+						dialogCategories : function() {
+							return self.dialogCategories;
+						},
+						user : function() {
+							return self.user;
+						},
+						loginp : function() {
+							return self.loginp;
+						}
+					}
+				});
 				
-				//$modal.data = {};
-				//lets check if the user is logged
-				user = steam.loginp();
-				
-				dialogOptions.loginp = steam.loginp;
-				
-				console.log('User ', user);
-				if(!!user){
-					dialogOptions.isLoged = true;
-				}else{
-					dialogOptions.isLoged = false;
-				}
-
-				this.showDialog(customDialogDefaults, customDialogOptions);
-			}
-		};
-
-		this.showDialog = function(customDialogDefaults, customDialogOptions) {
-			//Create temp objects to work with since we're in a singleton service
-			var tempDialogDefaults = {};
-			var tempDialogOptions = {};
-
-			//Map angular-ui modal custom defaults to modal defaults defined in this service
-			angular.extend(tempDialogDefaults, dialogDefaults, customDialogDefaults);
-
-			//Map modal.html $scope custom properties to defaults defined in this service
-			angular.extend(tempDialogOptions, dialogOptions, customDialogOptions);
-
-			if (!tempDialogDefaults.controller) {
-				tempDialogDefaults.controller = function($scope, modal) {
-					$scope.dialogOptions = tempDialogOptions;
+				modalInstance.result.then(function (selectedItem) {
 					
-					$scope.dialogOptions.close = function(result) {
-						modal.close(result);
-						self.isOpen = false;
-					};
-					$scope.dialogOptions.login = function(result) {
-						console.log('Trying to login...');
-						$rootScope.openlogin=true;
-						//loginCtrl.openLogin();
-						
-					};
-					$scope.dialogOptions.save = function(result) {
-						var jsonMsg = {
-							title : $scope.dialogOptions.title,
-							fullText : tinyMCE.get(elementID).getContent(),
-							labels : $scope.dialogOptions.labels
-						};
+				}, function () {
+					console.info('Modal dismissed at: ' + new Date());
+					self.isOpen = false;
+				});
+				
+				modalInstance.opened.then(function (selectedItem) {
+					console.log('modal opened');
+					self.waitForVisible()
+				});
+			};
+			var ModalInstanceCtrl = function($scope,$modalInstance, dialogCategories, user, loginp) {
+				$scope.dialogCategories = dialogCategories;
+				$scope.selectedCategory = $scope.dialogCategories[0];
 
-						//lets steam the information
-						steam.put('news', jsonMsg).then(function(){
-							console.log('Steam posted.....');
-						});
-					};
-					$scope.dialogOptions.post = function() {
-						var jsonMsg = {
-							title : $scope.dialogOptions.title,
-							fullText : tinyMCE.get(elementID).getContent(),
-							labels : $scope.dialogOptions.labels
-						};
+				$scope.user = user;
+				$scope.loginp = loginp;
 
-						//lets steam the information
-						steam.post('news', jsonMsg).then(function(){
-							console.log('Steam posted.....');
-							modal.close();
-							self.isOpen = false;
-						});
+				$scope.close = function(result) {
+					try{
+						$modalInstance.close();
+					} catch(e){
+						console.warn('Update bootstrap librarie to get ride of the error');
+					}
+				};
+				$scope.login = function(result) {
+					console.log('Trying to login...');
+					$rootScope.openlogin = true;
+				};
+				$scope.save = function(result) {
+					var jsonMsg = {
+						title : this.title,
+						fullText : tinyMCE.get('inputContentRichText').getContent(),
+						labels : this.labels,
+						category : this.selectedCategory
 					};
-					$scope.dialogOptions.formValid = function() {
-						return $scope.dialogOptions.title.length > 0;
+					// lets steam the information
+					steam.put('news', jsonMsg).then(function() {
+						console.log('Steam put.....');
+					});
+				};
+				$scope.post = function() {
+					var jsonMsg = {
+						title : this.title,
+						fullText : tinyMCE.get('inputContentRichText').getContent(),
+						labels : this.labels,
+						category : this.selectedCategory
 					};
+
+					// lets steam the information
+					steam.post('news', jsonMsg).then(function() {
+						console.log('Steam post.....');
+						try{
+							$modalInstance.close();
+						} catch(e){
+							console.warn('Update bootstrap librarie to get ride of the error');
+						}
+					});
+				};
+			};
+
+			self.waitForVisible = function() {
+				if($('.modal.myWindow').is(':visible')){
+					self.activateTinyMce();
+				}else{
+					setTimeout(function(){
+						self.waitForVisible();
+					},100);
 				}
-			}
-			var m = $modal;
-			m.open(tempDialogDefaults);
-			self.waitForVisible();
-		};
+			};
+			self.activateTinyMce = function() {
+				console.log('Activate TinyMce loaded....');
+				tinymce.init({
+							selector : "#inputContentRichText",
+							plugins : [
+									"advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+									"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+									"save table contextmenu directionality emoticons template paste textcolor" ],
+							width : '100%',
+							height : 310,
+							browser_spellcheck : true,
+							statusbar : false,
+							menubar : false,
+							paste_as_text : true,
+							toolbar : "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | link image media | bullist | forecolor backcolor",
+							charLimit : 100000, // this is a
+												// default
+												// value
+												// which can
+												// get
+												// modified
+												// later
+						});
+			};
 
+		} ]);
 
-		this.waitForVisible = function() {
-			if($('.modal.myWindow').is(':visible')){
-				self.activateTinyMce();
-			}else{
-				setTimeout(function(){
-					self.waitForVisible();
-				},100);
-			}
-		};
-
-		this.activateTinyMce = function() {
-			console.log('Activate TinyMce loaded....');
-			tinymce.init({
-				selector : "#" + elementID,
-				plugins: [
-					"advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
-					"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-					"save table contextmenu directionality emoticons template paste textcolor"
-				],
-				width : '100%',
-				height : 310,
-				browser_spellcheck : true,
-				statusbar : false,
-				menubar : false,
-				paste_as_text : true,
-				toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | link image media | bullist | forecolor backcolor", 
-				charLimit : 100000, // this is a default value which can get modified later
-			});
-		};
-	}
-]); 
+}).call(this);
