8def0f114977b4447455375beec762baf6861452
Paulo Almeida <paulo.cc03@gmail.com>
2013-12-05 00:50:20 +0700

    menu continuation. now comunicating throw  $rootScope

--- app/js/controllers/res-guides.js
+++ app/js/controllers/res-guides.js
@@ -2,8 +2,7 @@
 
 	var appModule = angular.module('TechGrindApp.controllers.res-guides', []);
 
-	appModule.controller('ResourcesGuidesCtrl', ['$scope', '$location', 'steam', '$filter',
-	function($scope, loc, steam, $filter) {
+	appModule.controller('ResourcesGuidesCtrl', ['$scope', '$location', 'steam', '$filter', '$rootScope', function($scope, loc, steam, $filter, $rootScope) {
 
 		// I flag the visibility of the big box.
 		$scope.isBoxVisible = true;
@@ -11,17 +10,120 @@
 		// ---
 		// PUBLIC METHODS.
 		// ---
-		$scope.activateSection = function(id){
+		$scope.activateSection = function(id) {
 			//First we must obtain the list from steam
 			//TODO Steam call to obtain list of categories
 			$scope.activeTitle = listOfCategories[id].title;
-			console.log('ACTIVE TITLE: ',$scope.activeTitle);
-			$scope.activeId =listOfCategories[id].id;
-			$scope.activeDescription =listOfCategories[id].description;
+			console.log('ACTIVE TITLE: ', $scope.activeTitle);
+			$scope.activeId = listOfCategories[id].id;
+			$scope.activeDescription = listOfCategories[id].description;
 		}
+
+		$rootScope.listOfCategories = listOfCategories;
 
 		//need to think in where I'm going to catch this id
 		return $scope.activateSection(0);
 	}]);
 
+	var listOfCategories = [
+			{
+				'title' : 'Architectural patterns',
+				'id' : '0',
+				'content' : 'An architectural pattern is a standard design in the field of software architecture. The concept of an architectural pattern has a broader scope than the concept of design pattern. The architectural patterns address various issues in software engineering, such as computer hardware performance limitations, high availability and minimization of a business risk. Some architectural patterns have been implemented within software frameworks.',
+				'items' : [{
+					id : '0',
+					name : 'first'
+				}, {
+					id : '1',
+					name : 'second'
+				}, {
+					id : '2',
+					name : 'last'
+				}]
+
+			}, {
+				'title' : 'Seed Round',
+				'id' : '1',
+				'content' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, ',
+				'items' : [{
+					id : '3',
+					name : '3'
+				}, {
+					id : '4',
+					name : '4'
+				}, {
+					id : '5',
+					name : '5'
+				}]
+			}, {
+				'title' : 'Investors',
+				'id' : '2',
+				'content' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, ',
+				'items' : [{
+					id : '6',
+					name : 'six'
+				}, {
+					id : '7',
+					name : 'seven'
+				}, {
+					id : '8',
+					name : 'eight'
+				}]
+			}, {
+				'title' : 'Plans',
+				'id' : '3',
+				'content' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, ',
+				'items' : [{
+					id : '9',
+					name : 'first'
+				}, {
+					id : '10',
+					name : 'second'
+				}, {
+					id : '11',
+					name : 'last'
+				}]
+			}, {
+				'title' : 'Budget',
+				'id' : '4',
+				'content' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, ',
+				'items' : [{
+					id : '12',
+					name : 'first'
+				}, {
+					id : '13',
+					name : 'second'
+				}, {
+					id : '14',
+					name : 'last'
+				}]
+			}, {
+				'title' : 'Offices',
+				'id' : '5',
+				'content' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, ',
+				'items' : [{
+					id : '15',
+					name : 'first'
+				}, {
+					id : '16',
+					name : 'second'
+				}, {
+					id : '17',
+					name : 'last'
+				}]
+			}, {
+				'title' : 'Hiring',
+				'id' : '6',
+				'content' : 'Cras orci turpis, interdum vitae felis volutpat, viverra malesuada nunc. Fusce magna dolor, ',
+				'items' : [{
+					id : '18',
+					name : 'first'
+				}, {
+					id : '19',
+					name : 'second'
+				}, {
+					id : '20',
+					name : 'last'
+				}]
+			}, ];
 }).call(this);
