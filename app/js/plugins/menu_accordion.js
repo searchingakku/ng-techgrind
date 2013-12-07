0dea9b0c4551d37db680a738a6cbd3a563eb4052
Paulo Almeida <paulo.cc03@gmail.com>
2013-12-07 14:06:34 +0700

    menu on guides semi working

--- app/js/plugins/menu_accordion.js
+++ app/js/plugins/menu_accordion.js
@@ -10,19 +10,22 @@
 		
 		if(lokky == 'docs'){
 			//listOfCatFromJson = $http.get('/json/docs_menu.json');
-			listOfCatFromJson = $http.get('http://dev-back1.techgrind.asia/scripts/rest.pike?request=/home/techgrind/resources/tree');
+			listOfCatFromJson = $http.get('http://dev-back1.techgrind.asia/scripts/rest.pike?request=/home/techgrind/resources/docs/tree');
 		}else if(lokky == 'guides'){
 			//listOfCatFromJson = $http.get('/json/guides_menu.json');
-			listOfCatFromJson = $http.get('http://dev-back1.techgrind.asia/scripts/rest.pike?request=/home/techgrind/resources/tree');
+			listOfCatFromJson = $http.get('http://dev-back1.techgrind.asia/scripts/rest.pike?request=/home/techgrind/resources/guides/tree');
 		}
 		
 		listOfCatFromJson.success(function(data){
 			console.log('Data for menu...', data.inventory);
-			$scope.menuItems = data.inventory[0].inventory;
+			$scope.menuItems = data.inventory;
 		});
 
 		$scope.selectedCat = $routeParams.cat;
 		$scope.selectedList = $routeParams.list;
+		$scope.modifyUrl = function(url){
+			return url.replace('/home/techgrind/','/#/');
+		};
 
 	}]);
 }).call(this);
