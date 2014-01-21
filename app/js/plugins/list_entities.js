467d8df5372c4f16468bbefedd86e64bc97d4fa6
thanakron@tandavas.com <thanakron@tandavas.com>
2014-01-22 01:29:20 +0700

    Added list entities

--- app/js/plugins/list_entities.js
+++ app/js/plugins/list_entities.js
@@ -24,13 +24,51 @@
 			investors/startups/services -> small logo thumbnail + detailed info ==> profile page
 */
 
+// - name
+// - location
+// - type ( startup, investors, conworking space etc...)
+// - ID for user : user.type.favorite[] 
+
+// row 1 : Name | location
+// row 2 : Type
+
+// Pagination or inifinity scroll (download, say, 5 rows at a time)
+// Add star on the right
+// click at the star = fixed on top (favorite) & add that entities  user.type.favorite[] for user record
+// click at the row = redirect to the start/service/investor profile page. example http://127.0.0.1:8000/#/people
+
 (function() {
 
 	var appModule = angular.module('TechGrindApp.controllers.list.entities', []);
-	
-	appModule.controller('ListStartupCtrl', ['$scope', 'steam',
-	function($scope, steam) {
+ 
+	appModule.controller('ListEntitiesCtrl', ['$scope', 'steam', '$routeParams',
+	  function($scope, steam, rp) {
+	 
+	    var get_countries, get_country;
+	    $scope.countries = {};
+	    $scope.sgenome = {};
+	    $scope.debug = [];
+	 
+	    get_country = function(country, filter) {
+	      $scope.debug.push = ["getting", country, filter];
+	      if (filter) {
+	        filter = "/" + filter;
+	      } else {
+	        filter = "";
+	      }
+	      return steam.get('/home/techgrind/organizations/country/' + country + filter).then(function(data) {
+	        $scope.debug.push = "got " + country;
+	        $scope.test = data.sgenome;
+	        console.log($scope.test);
+	        return $scope.entities = data.sgenome;
+	      });
+	    };
 
-	}]);
+	    if (rp.region) {
+	      return get_country(rp.region, rp.filter);
+	    }
+
+	    return $scope.entities;
+	  }]);
 
 }).call(this);
