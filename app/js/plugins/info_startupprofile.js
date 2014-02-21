a3d7d9231f5ed48890e1418292eb24314dd02033
thanakron@tandavas.com <thanakron@tandavas.com>
2014-02-21 22:05:42 +0700

    Simplified start profile page

--- app/js/plugins/info_startupprofile.js
+++ app/js/plugins/info_startupprofile.js
@@ -1,32 +1,19 @@
 var appModule = angular.module('TechGrindApp.controllers.info.startupprofile', []);
 
-appModule.controller('InfoStartUpProfileCtrl', ['$scope', 'steam', '$http', '$rootScope',
-function($scope, steam, http, $rootScope) {
+appModule.controller('InfoStartUpProfileCtrl', ['$scope', 'steam', '$http', '$rootScope', '$location',
+function($scope, steam, http, $rootScope, loc) {
 
-	$scope.maintabs = maintabs;
+	$scope.startupprofile = startupprofile;
+	$scope.demo = demo;
+	$scope.news = news;
+	$scope.fundraising = fundraising;
 	$scope.sidetabs = sidetabs;
 
+	$scope.goToPeople = function(userid) {
+    	loc.path('profile/people/' + userid);
+    }
+
 }]);
-
-// function ProfileTabCtrl($scope){
-// 	$scope.content = 'Profile';
-// }
-
-// function NewsTabCtrl($scope){
-// 	$scope.content = 'News';
-// }
-
-// function JobsTabCtrl($scope){
-// 	$scope.content = 'Jobs';
-// }
-
-// function ContentTabCtrl($scope){
-// 	$scope.content = 'Content';
-// }
-
-// function FundraisingCtrl($scope){
-// 	$scope.content = 'Fundraising';
-// }
 
 var investment = [{
 	date: '26/10/2013',
@@ -51,9 +38,7 @@ var jobs = [{
 	description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis ante non massa sollicitudin adipiscing. Quisque ornare lectus non tincidunt porta. Phasellus iaculis sit amet nunc eu tincidunt. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce eleifend quam quis est scelerisque vehicula. Donec vehicula velit felis, ut interdum felis laoreet lacinia. Mauris luctus, nunc at imperdiet sodales, nisi lorem rhoncus turpis, et mattis ligula lectus eget enim.'
 }];
 
-var maintabs = [{
-	title: 'Profile',
-	// controller: ProfileTabCtrl,
+var startupprofile = {
 	startupid: 'techgrind',
 	name: 'TechGrind',
 	location: 'Thailand',
@@ -65,18 +50,17 @@ var maintabs = [{
 	facebook: 'https://www.facebook.com/techgrind',
 	twitter: 'https://twitter.com/Tech_Grind',
 	followers: 479
-}, {
-	title: 'Demo',
-	// controller: ContentTabCtrl,
+};
+var demo = {
+	title: 'TechGrind 1.0',
 	description: 'TechGrind is Asia\'s premier startup ecosystem. Made by startups, for startups. We are both an online and offline community with the sole focus of enabling entrepreneurs from all walks of life to succeed in building their ventures. From pitching contests, hackathons, workshops, and the strongest entrepreneurship network in Asia with global ties to the strongest networks around the world, TechGrind is all about helping people build amazing things and take ideas from concept to consumer.'
-}, {
-	title: 'News',
-	// controller: NewsTabCtrl,
-}, {
-	title: 'Fundraising',
-	// controller: FundraisingCtrl,
-	fundraising: investment
-}];
+};
+var news = {
+	title: 'News'
+};
+var fundraising = {
+	investment: investment
+};
 
 var sidetabs = [{
 	title: 'Jobs',
